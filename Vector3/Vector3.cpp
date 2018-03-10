e46ed3511171711ca4d2c9ba8496f211cbc98049
AnatoliyGorkov <31771671+AnatoliyGorkov@users.noreply.github.com>
2018-03-10 23:42:08 +0300

    fixed

--- Vector3/Vector3.cpp
+++ Vector3/Vector3.cpp
@@ -1,81 +1,73 @@
 ﻿#include "Vector3.h"
 
-Vector3::Vector3():
-	x_(0),
-	y_(0),
-	z_(0)
+Vector3::Vector3() :
+	x(0),
+	y(0),
+	z(0)
 {}
 
-Vector3::Vector3(double x, double y, double z):
-	x_(x),
-	y_(y),
-	z_(z)
+Vector3::Vector3(double x, double y, double z) :
+	x(x),
+	y(y),
+	z(z)
 
 {}
-
-Vector3::Vector3(const Vector3 & other):
-	x_(other.x_),
-	y_(other.y_),
-	z_(other.z_)
-{}
-
-Vector3::~Vector3()
-{}
-
-const Vector3 & Vector3::operator=(const Vector3 & other)
-{
-	x_ = other.x_;
-	y_ = other.y_;
-	z_ = other.z_;
-	return *this;
-}
 
 Vector3 Vector3::operator+(const Vector3 & other) const
 {
-	return Vector3(x_ + other.x_, y_ + other.y_, z_ + other.z_);
+	return Vector3(x + other.x, y + other.y, z + other.z);
 }
 
 Vector3 & Vector3::operator+=(const Vector3 & other)
 {
-	x_ += other.x_;
-	y_ += other.y_;
-	z_ += other.z_;
+	x += other.x;
+	y += other.y;
+	z += other.z;
 	return *this;
 }
 
+
 Vector3 Vector3::operator-(const Vector3 & other) const
 {
-	return Vector3(x_ - other.x_, y_ - other.y_, z_ - other.z_);
+	return Vector3(x - other.x, y - other.y, z - other.z);
 }
 
 Vector3 & Vector3::operator-=(const Vector3 & other)
 {
-	x_ -= other.x_;
-	y_ -= other.y_;
-	z_ -= other.z_;
+	x -= other.x;
+	y -= other.y;
+	z -= other.z;
+	return *this;
+}
+
+Vector3& Vector3::invert()
+{
+	x = -x;
+	y = -y;
+	z = -z;
 	return *this;
 }
 
 Vector3 Vector3::operator-() const
 {
-	return Vector3(-x_, -y_, -z_);
+	return Vector3(-x, -y, -z);
 }
 
 Vector3 Vector3::operator*(double k) const
 {
-	return Vector3(k * x_, k * y_, k * z_);
+	return Vector3(k * x, k * y, k * z);
 }
 
 double Vector3::operator*(const Vector3 & other) const
 {
-	return x_ * other.x_ + y_ * other.y_ + z_ * other.z_;
+	return x * other.x + y * other.y + z * other.z;
 }
 
 Vector3 Vector3::operator^(const Vector3 & other) const
 {
-	return Vector3(y_ * other.z_ - z_ * other.y_,
-					z_ * other.x_ - x_ * other.z_,
-					x_ * other.y_ - y_ * other.x_);
+	return Vector3(y * other.z - z * other.y,
+		z * other.x - x * other.z,
+		x * other.y - y * other.x);
 }
 
 double Vector3::squareLen() const
@@ -96,54 +88,49 @@ Vector3 & Vector3::norm()
 	Лучше не сравнивать явно с нулем.
 	http://ru.cppreference.com/w/cpp/types/numeric_limits/epsilon
 	*/
-	if (len == 0)
+	if (almostEqual(len, 0.0))
 		return (*this);
-	x_ /= len;
-	y_ /= len;
-	z_ /= len;
+	x /= len;
+	y /= len;
+	z /= len;
 	return (*this);
 }
 
 Vector3 & Vector3::rotate(double angle, const Vector3 & axis)
 {
-	if (axis.len() == 0)	//How we can choose eps value to compare with (shouldn't compare double with 0)?
+	if (almostEqual(axis.len(), 0.0))
 		return *this;
-	double x, y, z;
+	Vector3 axisNorm = axis;
+	axisNorm.norm();
 	double cosA = std::cos(angle);
 	double sinA = std::sin(angle);
-	x = (cosA + (1 - cosA) * axis.x_ * axis.x_) * x_ +
-		((1 - cosA) * axis.x_ * axis.y_ - sinA * axis.z_) * y_ +
-		((1 - cosA) * axis.x_ * axis.z_ + sinA * axis.y_) * z_;
-
-	y = ((1 - cosA) * axis.y_ * axis.x_ + sinA * axis.z_) * x_ +
-		(cosA + (1 - cosA) * axis.y_ * axis.y_) * y_ +
-		((1 - cosA) * axis.y_ * axis.z_ - sinA * axis.x_) * z_;
-
-	z = ((1 - cosA) * axis.z_ * axis.x_ - sinA * axis.y_) * x_ +
-		((1 - cosA) * axis.z_ * axis.y_ + sinA * axis.x_) * y_ +
-		(cosA + (1 - cosA) * axis.z_ * axis.z_) * z_;
-	
 	/*
-	покомпонентные операции выглядят жестко ... я тут в закромах нашел такой вариант
-	ваш вариант конечно тоже подойдет.
-    
-  void Rotate(const Vector3 &axis, const T sinAng, const T cosAng)
-  {
-    Vector3<T> self = *this;
-    Vector3<T> x = self - axis * (axis * self);
-    Vector3<T> y = x ^ axis;
-    Vector3<T> delta = x * cosAng + y * sinAng - x;
-    self += delta;
-    *this = self;
-  }
-  void  Rotate(const Vector3 &axis, const T angle)
-  {
-    Rotate(axis, sin(angle), cos(angle));
-  }
+	double x_, y_, z_;
+
+	x_ = (cosA + (1 - cosA) * axisNorm.x * axisNorm.x) * x +
+		((1 - cosA) * axisNorm.x * axisNorm.y - sinA * axisNorm.z) * y +
+		((1 - cosA) * axisNorm.x * axisNorm.z + sinA * axisNorm.y) * z;
+
+	y_ = ((1 - cosA) * axisNorm.y * axisNorm.x + sinA * axisNorm.z) * x +
+		(cosA + (1 - cosA) * axisNorm.y * axisNorm.y) * y +
+		((1 - cosA) * axisNorm.y * axisNorm.z - sinA * axisNorm.x) * z;
+
+	z_ = ((1 - cosA) * axisNorm.z * axisNorm.x - sinA * axisNorm.y) * x +
+		((1 - cosA) * axisNorm.z * axisNorm.y + sinA * axisNorm.x) * y +
+		(cosA + (1 - cosA) * axisNorm.z * axisNorm.z) * z;
+
+	x = x_;
+	y = y_;
+	z = z_;
 	*/
-	x_ = x;
-	y_ = y;
-	z_ = z;
+
+	Vector3 self = *this;
+	Vector3 a = self - axisNorm * (axisNorm * self);
+	Vector3 b = a ^ axisNorm;
+	Vector3 delta = a * cosA + b * sinA - a;
+	self += delta;
+	*this = self;
+
 	return *this;
 }
 
@@ -158,7 +145,7 @@ double Vector3::angle(const Vector3 & other) const
 {
 	double len1 = (*this).len();
 	double len2 = other.len();
-	if (len1 == 0 || len2 == 0)	//same question as in rotate()
+	if (almostEqual(len1, 0.0) || almostEqual(len2, 0.0))	//same question as in rotate()
 		return 0;
 	return std::acos((*this) * other / len1 / len2);
 }
@@ -170,12 +157,17 @@ Vector3 operator*(double k, const Vector3 & v)
 
 std::ostream & operator<<(std::ostream & stream, const Vector3 & v)
 {
-	stream << "(" << v.x_ << " " << v.y_ << " " << v.z_ << ")";
+	stream << "(" << v.x << " " << v.y << " " << v.z << ")";
 	return stream;
 }
 
 std::istream & operator>>(std::istream & stream, Vector3 & v)
 {
-	stream >> v.x_ >> v.y_ >> v.z_;
+	stream >> v.x >> v.y >> v.z;
 	return stream;
+}
+
+bool almostEqual(double lhs, double rhs)
+{
+	return std::abs(lhs - rhs) <= std::numeric_limits<double>::epsilon() * std::max(std::abs(lhs), std::abs(rhs));
 }
